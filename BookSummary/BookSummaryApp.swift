77d3b494afcc811bc128acc9e16c30da781624df
Volodymyr Matiukh <vova.matiukh@gmail.com>
2023-12-05 23:11:54 +0200

    Initial Commit

---
+++ BookSummary/BookSummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSummaryApp.swift
+//  BookSummary
+//
+//  Created by Volodymyr Matiukh on 05.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookSummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookSummary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookSummary
+//
+//  Created by Volodymyr Matiukh on 05.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
